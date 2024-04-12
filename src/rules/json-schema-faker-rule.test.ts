c16275dbe6857fe9d4575ff305c2acb5792e9702
Shane Jonas <jonas.shane@gmail.com>
2024-04-12 11:52:39 -0400

    fix: add getExampleCalls to JsonSchemaFaker tests to get more coverage

--- src/rules/json-schema-faker-rule.test.ts
+++ src/rules/json-schema-faker-rule.test.ts
@@ -4,17 +4,35 @@ import JsonSchemaFakerRule from "./json-schema-faker-rule";
 describe("JsonSchemaFakerRule", () => {
   it("should validate example calls", () => {
     const rule = new JsonSchemaFakerRule();
-    const exampleCall = {
-      title: 'test call',
-      methodName: "foo",
-      params: [],
-      url: "http://localhost:3333",
-      resultSchema: {
-        type: "boolean",
+    const openrpcDocument = {
+      openrpc: "1.0.0",
+      info: {
+        title: "my api",
+        version: "0.0.0-development",
       },
-      result: true,
-    };
-    const result = rule.validateExampleCall(exampleCall);
+      servers: [
+        {
+          name: "my api",
+          url: "http://localhost:3333",
+        },
+      ],
+      methods: [
+        {
+          name: "foo",
+          params: [],
+          paramStructure: "by-name",
+          result: {
+            name: "fooResult",
+            schema: {
+              type: "boolean",
+            },
+          },
+        },
+      ],
+    } as any;
+    const calls = rule.getExampleCalls(openrpcDocument, openrpcDocument.methods[0]);
+    calls[0].result = true;
+    const result = rule.validateExampleCall(calls[0]);
     expect(result.valid).toBe(true);
   });
   it("should handle errors within ajv when validating", () => {
